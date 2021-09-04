caa5a75970067bf2229c075c25434d2f861c9836
rubenderigo <rm.derigo@gmail.com>
2021-09-04 14:50:47 -0300

    delete initial files and add migratios

---
+++ database/migrations/2021_09_04_163713_create_reports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->enum('type_manner', ['buen trato', 'trato normal', 'mal trato']);
+            $table->enum('type_report', ['opinión', 'denuncia']);
+            $table->string('description');
+            $table->integer('assessment')->default(1);
+            $table->integer('num_reports')->default(0);
+            $table->tinyInteger('active')->default(0);
+            $table->unsignedBigInteger('id_place');
+            $table->foreign('id_place')->references('id')->on('places');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,4 @@
 <?php
 
-use App\Http\Controllers\PersonController;
-use App\Http\Controllers\PlaceController;
-use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/person', PersonController::class);
-
-Route::resource('/place', PlaceController::class);
-
-Route::resource('/report', ReportController::class);
