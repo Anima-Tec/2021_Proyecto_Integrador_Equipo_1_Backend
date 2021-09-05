7b658c0835f10591a1c2c166915bf687e59d71d7
rubenderigo <rm.derigo@gmail.com>
2021-09-05 19:01:08 -0300

    refactor and add new models

---
+++ app/Models/Report.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory;
+    protected $table = 'reports';
+    protected $fillable = [
+        'id',
+        'date',
+        'type_report',
+        'description',
+        'assessment',
+        'num_reports',
+        'active',
+        'id_place'
+    ];
+}
