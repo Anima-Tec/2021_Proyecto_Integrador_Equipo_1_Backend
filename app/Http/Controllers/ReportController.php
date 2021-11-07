25e455764619c2a6799f9409b21cdc5d32efbc38
rubenderigo <rm.derigo@gmail.com>
2021-11-07 17:09:31 -0300

    add person profile to reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -16,54 +16,32 @@ class ReportController extends ApiController
 
     public function indexAdmin()
     {
-        $Reports = Report::where('active', 2)
-            ->select('id', 'date', 'type_report', 'description', 'assessment', 'photo')
-            ->get();
+        $Reports = $this->getReports(['reports.active', 2]);
         return $this->sendResponse($Reports, 200);
     }
 
     public function indexPlace($address)
     {
         $Place = Place::where('address', $address)->first();
-        $Reports = Report::where([['rd.id_place', $Place->id], ['reports.active', 1]])
-            ->select('reports.id', 'type_report', 'description', 'assessment', 'photo', 'address', 'rd.created_at as date', 'username')
-            ->join('places', 'reports.id_place', '=', 'places.id')
-            ->join('reports_created as rd', 'reports.id', '=', 'rd.id_report',)
-            ->join('users', 'users.id', '=', 'rd.id_person')
-            ->get();
+        $Reports = $this->getReports([['rc.id_place', $Place->id], ['reports.active', 1]]);
         return $this->sendResponse($this->generateDateAgo($Reports), 200);
     }
 
     public function indexPerson($id)
     {
-        $Reports = Report::where([['rd.id_person', $id], ['reports.active', 1]])
-            ->select('reports.id', 'type_report', 'description', 'assessment', 'photo', 'address', 'rd.created_at as date', 'username')
-            ->join('places', 'reports.id_place', '=', 'places.id')
-            ->join('reports_created as rd', 'reports.id', '=', 'rd.id_report',)
-            ->join('users', 'users.id', '=', 'rd.id_person')
-            ->get();
+        $Reports = $this->getReports([['rc.id_person', $id], ['reports.active', 1]]);
         return $this->sendResponse($this->generateDateAgo($Reports), 200);
     }
 
     public function index()
     {
-        $Reports = Report::where('reports.active', 1)
-            ->select('reports.id', 'type_report', 'description', 'assessment', 'photo', 'address', 'rd.created_at as date', 'username')
-            ->join('places', 'reports.id_place', '=', 'places.id')
-            ->join('reports_created as rd', 'reports.id', '=', 'rd.id_report',)
-            ->join('users', 'users.id', '=', 'rd.id_person')
-            ->get();
+        $Reports = $this->getReports([['reports.active', 1]]);
         return $this->sendResponse($this->generateDateAgo($Reports), 200);
     }
 
     public function show($id)
     {
-        $Report = Report::where([['reports.id', $id], ['reports.active', 1]])
-            ->select('reports.id', 'type_report', 'description', 'assessment', 'photo', 'address', 'rd.created_at as date', 'username')
-            ->join('places', 'reports.id_place', '=', 'places.id')
-            ->join('reports_created as rd', 'reports.id', '=', 'rd.id_report',)
-            ->join('users', 'users.id', '=', 'rd.id_person')
-            ->get();
+        $Report = $this->getReports([['reports.id', $id], ['reports.active', 1]]);
 
         if ($Report) {
             return $this->sendResponse($this->generateDateAgo($Report), 200);
@@ -93,7 +71,7 @@ class ReportController extends ApiController
                 'id_place' => $idPlace,
                 'photo' => $this->createPathPhoto($request, "reports"),
             ]);
-            
+
             DB::table('reports_created')
                 ->insert([
                     'id_report' => $Report->id,
